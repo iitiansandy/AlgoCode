340044fa9f5c7eb09167275bf73ea237ddbd47cb
iitiansandy <sandeep.sid.kumar10@gmail.com>
2024-04-09 23:23:17 +0530

    Feat: Added routing structure for versioned api for porblems

--- src/index.js
+++ src/index.js
@@ -3,12 +3,17 @@ const bodyParser = require('body-parser');
 
 
 const { PORT } = require('./config/server.config');
+const apiRouter = require('./routes');
 
 
 const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true}));
 app.use(bodyParser.text());
+
+app.use('/api', apiRouter);
+
+
 
 app.get('/ping', (req, res) => {
     return res.json({message: 'Problem service is alive'})
