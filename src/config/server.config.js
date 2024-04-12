a0837f266bf033e144c2be829b280e70512d33c4
iitiansandy <sandeep.sid.kumar10@gmail.com>
2024-04-12 18:23:30 +0530

    Feat: Added MongoDB config

--- src/config/server.config.js
+++ src/config/server.config.js
@@ -4,4 +4,6 @@ dotenv.config();
 
 module.exports = {
     PORT: process.env.PORT || 3000,
+    MongoDBURL: process.env.MONGO_DB_URL,
+    NODE_ENV: process.env.NODE_ENV || "development"
 }
--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ const { PORT } = require('./config/server.config');
 const apiRouter = require('./routes');
 const BaseError = require('./errors/base.error');
 const errorHandler = require('./utils/errorHandler');
+const connectToDB = require('./config/db.config');
 
 
 const app = express();
@@ -26,8 +27,9 @@ app.get('/ping', (req, res) => {
 // Last middleware if any error comes
 app.use(errorHandler);
 
-app.listen(PORT, () => {
+app.listen(PORT, async() => {
     console.log('Server started at PORT', PORT);
 
-    
+    await connectToDB();
+    console.log("Database connected");
 })
